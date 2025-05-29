1e765160a802094ef121fa162c86e5bbc1ba81ce
Carlos Ravelo <ravelo.carlos@gmail.com>
2025-05-28 21:02:50 -0400

    Added firebase
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjVO-EIYN7N6sT4hEBxlNTA4Zo9nqJrsw",
+  authDomain: "chores-25a18.firebaseapp.com",
+  projectId: "chores-25a18",
+  storageBucket: "chores-25a18.firebasestorage.app",
+  messagingSenderId: "965302916763",
+  appId: "1:965302916763:web:7bc4760f8e8b105599583e",
+  measurementId: "G-VS8FKP4WHN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
